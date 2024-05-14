b6dce6de0b469327a1203a23fa7187b63732c307
caiolima <caiolima@fieg.com.br>
2024-05-14 13:41:00 -0300

    Formatação de valores

--- app/Scripts/scrapperRappi.py
+++ app/Scripts/scrapperRappi.py
@@ -7,12 +7,15 @@ import concurrent.futures
 
 rappi_root = f'https://www.rappi.com.br/'
 headers = {
-    'User-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"}
-
+    'User-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)"
+                  " Chrome/124.0.0.0 Safari/537.36"}
 
 
 def extrair_html(url, header):
+    start_time = time.perf_counter()
     site = requests.get(url, headers=header)
+    end_time = time.perf_counter()
+    tempo_extracao = end_time - start_time
     soup = BeautifulSoup(site.content, 'html.parser')
     return soup
 
@@ -32,9 +35,16 @@ def getMercados():
             mercadosBuscados.add(nome_mercado)
 
     end_time = time.perf_counter()
-    print(f"Tempo para buscar os links: {end_time - start_time} segundos")
-    return urlsRappi
+    tempo_total_mercados = end_time - start_time
+    print(f"Tempo para buscar os links: {tempo_total_mercados} segundos")
+    print(f"")
+    return urlsRappi, tempo_total_mercados
 
+def converter_preco(preco):
+    partes = preco.split(".")
+    if len(partes) > 2:
+        preco = "".join(partes[:-1]) + "." + partes[-1]
+    return preco.replace("R$", "").replace(",", ".").strip()
 
 def buscar_produtos(url):
     start_time = time.perf_counter()
@@ -43,8 +53,9 @@ def buscar_produtos(url):
     paginas = extrair_html(urlDefault, headers)
     leftMenus = paginas.find_all('ul', attrs={"data-qa": "corridor-list"})
 
-    mercadoVinculado = paginas.find('h1', attrs={"data-qa": "store-name"}).text.strip()
-    imagemMercado = paginas.find('img', alt=mercadoVinculado).get('src')
+    mercadoVinculado = paginas.find('h1', attrs={"data-qa": "store-name"})
+    imagemMercado = paginas.find('img', alt=mercadoVinculado.text.strip()).get('src') if mercadoVinculado else None
+    mercadoVinculado = mercadoVinculado.text.strip() if mercadoVinculado else None
 
     produtosList = {
         'nome': [],
@@ -65,7 +76,8 @@ def buscar_produtos(url):
                 categoria = leftMenuHref.text.strip()
 
                 try:
-                    verMaisList = extrair_html(newUrl, headers).find_all(
+                    paginasVerMais = extrair_html(newUrl, headers)
+                    verMaisList = paginasVerMais.find_all(
                         attrs={'data-qa': re.compile(r'store-corridors-list-aisle.*')})
 
                     if verMaisList:
@@ -73,22 +85,24 @@ def buscar_produtos(url):
                             links = verMais.find('a').get('href')
                             urlVerMais = rappi_root + links
 
-                            paginasVerMais = extrair_html(urlVerMais, headers).find_all(
+                            paginasVerMais = extrair_html(urlVerMais, headers)
+                            paginasVerMais = paginasVerMais.find_all(
                                 attrs={'data-qa': re.compile(r'product-item.*')})
 
-                            subcategoriaProdutos = verMais.find('h2', attrs={'data-qa': 'corridor-carrousel-title-slider'})
+                            subcategoriaProdutos = (verMais.find(attrs={'data-qa': 'corridor-carrousel-title-slider'})
+                                                    .text.strip())
                             for pagina in paginasVerMais:
                                 try:
                                     preco = pagina.find(attrs={'data-qa': 'product-price'}).text.strip()
                                     nome = pagina.find(attrs={'data-qa': 'product-name'}).text.strip()
                                     descricao = pagina.find(attrs={'data-qa': 'product-description'}).text.strip()
-                                    imagem = pagina.find('img').get('src')
+                                    imagem = pagina.find(attrs={'data-testid': 'image'}).get('src')
 
                                     if "/" in preco:
                                         valor, unidade = preco.split("/")
-                                        valor = valor.replace("R$", "").replace(",", ".").strip()
+                                        valor = converter_preco(valor)
                                     else:
-                                        valor = preco.replace("R$", "").replace(",", ".").strip()
+                                        valor = converter_preco(preco)
                                         unidade = None
 
                                     produtosList['preco'].append(valor)
@@ -112,11 +126,17 @@ def buscar_produtos(url):
     df.to_csv(f'rappi_{url.replace("/", "_")}.csv', encoding='utf-8', sep=';', index=False)
 
     end_time = time.perf_counter()
-    print(f"Tempo para buscar os produtos {url}: {end_time - start_time} segundos")
+    tempo_total = end_time - start_time
+    print(f"Tempo para buscar os produtos {url}: {tempo_total} segundos")
+    return tempo_total
 
 
 if __name__ == '__main__':
-    urlsRappi = getMercados()
+    urlsRappi, tempo_total_mercados = getMercados()
 
     with concurrent.futures.ProcessPoolExecutor() as executor:
-        executor.map(buscar_produtos, urlsRappi)
+        tempos = executor.map(buscar_produtos, urlsRappi)
+
+    tempo_total_requisicoes = sum(tempos) + tempo_total_mercados
+    print(f"Tempo total: {tempo_total_requisicoes} segundos")
+    print(f"Tempo gasto com requisições: {tempo_total_mercados} segundos")
